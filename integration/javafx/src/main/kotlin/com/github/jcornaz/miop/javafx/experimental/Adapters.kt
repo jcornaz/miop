60dc9dd9b082b8ade2c863e8822bd88acb626f97
Jonathan Cornaz <jonathan.cornaz@gmail.com>
2018-05-24 22:38:13 +0200

    Comment JavaFx Adapters

--- integration/javafx/src/main/kotlin/com/github/jcornaz/miop/javafx/experimental/Adapters.kt
+++ integration/javafx/src/main/kotlin/com/github/jcornaz/miop/javafx/experimental/Adapters.kt
@@ -7,7 +7,21 @@ import javafx.beans.value.ObservableValue
 import kotlinx.coroutines.experimental.javafx.JavaFx
 import kotlinx.coroutines.experimental.withContext
 
+/**
+ * Return a [SubscribableValue] backed by this [ObservableValue]
+ *
+ * Any change of the [ObservableValue] value would be notified to open subscriptions
+ */
 public fun <T> ObservableValue<out T>.asSubscribableValue(): SubscribableValue<T?> = ObservableValueAdapter(this)
+
+
+/**
+ * Return a [SubscribableVariable] backed by this property
+ *
+ * Any change in the [Property] would be notified to open subscriptions
+ *
+ * Any change in this [SubscribableVariable] would be transmitted to the property (notifying listeners of the property)
+ */
 public fun <T> Property<T>.asSubscribableVariable(): SubscribableVariable<T?> = PropertyAdapter(this)
 
 private class ObservableValueAdapter<out T>(private val observable: ObservableValue<out T>) : SubscribableValue<T?> {
