25e66b509fa94d74f502577fe611bd0a0698938e
Jonathan Cornaz <jonathan.cornaz@gmail.com>
2018-05-24 22:41:24 +0200

    Fix typo in adapter test

--- integration/javafx/src/test/kotlin/com/github/jcornaz/miop/javafx/experimental/AdapterTest.kt
+++ integration/javafx/src/test/kotlin/com/github/jcornaz/miop/javafx/experimental/AdapterTest.kt
@@ -47,7 +47,7 @@ class AdapterTest {
     }
 
     @Test
-    fun `Property#asSubscribableVariable should return an adapter reflecting the reflect the property`() = runBlocking {
+    fun `Property#asSubscribableVariable should return an adapter reflecting the property`() = runBlocking {
         val property = SimpleIntegerProperty(1)
         val subscribable = property.asSubscribableVariable()
 
