e134ca349979c7a00158428d33395644059ad60c
Jonathan Cornaz <jcornaz@users.noreply.github.com>
2018-07-19 17:48:28 +0200

    Update StateStore.kt

--- core/common/src/test/kotlin/com/github/jcornaz/miop/experimental/property/StateStore.kt
+++ core/common/src/test/kotlin/com/github/jcornaz/miop/experimental/property/StateStore.kt
@@ -4,67 +4,62 @@ import kotlinx.coroutines.experimental.Unconfined
 import kotlinx.coroutines.experimental.channels.*
 import kotlinx.coroutines.experimental.launch
 
-public interface StateStore<S, in A : (S) -> S> : SubscribableValue<S> {
-    fun dispatch(action: A)
+public interface StateStore<S, in A> : SubscribableValue<S> {
+  fun dispatch(action: A)
 }
 
-public fun <S, A : (S) -> S> StateStore(
-        initialState: S,
-        dispatchError: (S, Throwable) -> S
-): StateStore<S, A> = StateStoreImpl(initialState, dispatchError)
-
-private class StateStoreImpl<S, in A : (S) -> S>(
-        initialState: S,
-        private val dispatchError: (S, Throwable) -> S
+public class SimpleStateStore<S, in A : (S) -> S>(
+    initialState: S,
+    onError: (error: Throwable) -> A
 ) : StateStore<S, A> {
 
-    private val broadcast = ConflatedBroadcastChannel(initialState)
+  private val broadcast = ConflatedBroadcastChannel(initialState)
+  private val pendingActions = Channel<A>(Channel.UNLIMITED)
 
-    private val pendingActions = Channel<A>(Channel.UNLIMITED)
+  init {
+    launch(Unconfined) {
+      var state = initialState
 
-    init {
-        launch(Unconfined) {
-            var state = initialState
-
-            pendingActions.consumeEach {
-                try {
-                    val newState = it(state)
-                    if (newState !== state) {
-                        broadcast.send(newState)
-                        state = newState
-                    }
-                } catch (t: Throwable) {
-                    state = dispatchError(state, t)
-                }
-            }
+      pendingActions.consumeEach { action ->
+        val newState = try {
+          action(state)
+        } catch (error: Throwable) {
+          onError(error)(state)
         }
+
+        if (newState !== state) {
+          broadcast.send(newState)
+          state = newState
+        }
+      }
     }
+  }
 
-    override suspend fun get(): S = broadcast.value
+  override suspend fun get(): S = broadcast.value
 
-    override fun dispatch(action: A) {
-        pendingActions.offer(action)
-    }
+  override fun dispatch(action: A) {
+    pendingActions.offer(action)
+  }
 
-    override fun openSubscription(): ReceiveChannel<S> = broadcast.openSubscription()
+  override fun openSubscription(): ReceiveChannel<S> = broadcast.openSubscription()
 }
 
-private class StateStoreMapImpl<SS, SR, AS : (SS) -> SS, AR : (SR) -> SR>(
-        private val sourceStore: StateStore<SS, AS>,
-        private val transformState: (SS) -> SR,
-        private val transformAction: (AR) -> AS
+private class StateStoreMap<SS, SR, AS : (SS) -> SS, AR : (SR) -> SR>(
+    private val sourceStore: StateStore<SS, AS>,
+    private val transformState: (SS) -> SR,
+    private val transformAction: (AR) -> AS
 ) : StateStore<SR, AR> {
 
-    override fun dispatch(action: AR) = sourceStore.dispatch(transformAction(action))
+  override fun dispatch(action: AR) = sourceStore.dispatch(transformAction(action))
 
-    override suspend fun get(): SR = transformState(sourceStore.get())
+  override suspend fun get(): SR = transformState(sourceStore.get())
 
-    override fun openSubscription(): ReceiveChannel<SR> =
-            sourceStore.openSubscription()
-                    .map { transformState(it) }
+  override fun openSubscription(): ReceiveChannel<SR> =
+      sourceStore.openSubscription()
+          .map { transformState(it) }
 }
 
 public fun <SS, SR, AS : (SS) -> SS, AR : (SR) -> SR> StateStore<SS, AS>.map(
-        transformState: (SS) -> SR,
-        transformAction: (AR) -> AS
-): StateStore<SR, AR> = StateStoreMapImpl(this, transformState, transformAction)
+    transformState: (SS) -> SR,
+    transformAction: (AR) -> AS
+): StateStore<SR, AR> = StateStoreMap(this, transformState, transformAction)
