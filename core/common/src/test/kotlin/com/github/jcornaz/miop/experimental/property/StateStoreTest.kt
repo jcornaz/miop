653bc7e690960729a15cd1fb66745d71d27198d1
Jonathan Cornaz <jonathan.cornaz@gmail.com>
2018-07-19 20:40:44 +0200

    Add more tests

--- core/common/src/test/kotlin/com/github/jcornaz/miop/experimental/property/StateStoreTest.kt
+++ core/common/src/test/kotlin/com/github/jcornaz/miop/experimental/property/StateStoreTest.kt
@@ -3,9 +3,10 @@ package com.github.jcornaz.miop.experimental.property
 import com.github.jcornaz.miop.internal.test.AsyncTest
 import com.github.jcornaz.miop.internal.test.runTest
 import kotlinx.coroutines.experimental.Unconfined
-import kotlinx.coroutines.experimental.async
-import kotlinx.coroutines.experimental.channels.take
-import kotlinx.coroutines.experimental.channels.toList
+import kotlinx.coroutines.experimental.channels.first
+import kotlinx.coroutines.experimental.launch
+import kotlinx.coroutines.experimental.timeunit.TimeUnit
+import kotlinx.coroutines.experimental.withTimeout
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
@@ -16,11 +17,60 @@ class StateStoreTest : AsyncTest() {
         val store = StateStore(-1)
 
         assertEquals(-1, store.get())
-        val result = async(Unconfined) { store.openSubscription().take(2).toList() }
+
+        expect(1)
+        launch(Unconfined) {
+            val sub = store.openSubscription()
+            assertEquals(-1, sub.receive())
+
+            expect(2)
+
+            assertEquals(42, sub.receive()) // suspend
+
+            expect(4)
+        }
+
+        expect(3)
 
         store.dispatch { assertEquals(-1, it); 42 }
 
-        assertEquals(listOf(-1, 42), result.await())
+        expect(5)
+
         assertEquals(42, store.get())
+
+        finish(6)
+    }
+
+    @Test
+    fun subscriptionShouldAlwaysImmediatelyStartWithCurrentState() = runTest {
+        val store = StateStore("Hello world")
+
+        withTimeout(1, TimeUnit.SECONDS) {
+            assertEquals("Hello world", store.get())
+            assertEquals("Hello world", store.openSubscription().first())
+            assertEquals("Hello world", store.openSubscription().first())
+            assertEquals("Hello world", store.openSubscription().first())
+        }
+    }
+
+    @Test
+    fun subscriptionShouldNotReceiveUnchangedState() {
+        val store = StateStore("Hello")
+
+        expect(1)
+        launch(Unconfined) {
+            val sub = store.openSubscription()
+            assertEquals("Hello", sub.receive())
+            expect(2)
+            assertEquals("World", sub.receive()) // suspend
+            expect(5)
+        }
+
+        expect(3)
+        store.dispatch { it } // should not resume the job
+        expect(4)
+
+        store.dispatch { "World" } // should resume the job
+        finish(6)
     }
 }
