f21ec590d8f2546e209a5e11be87862409c7728b
Jonathan Cornaz <jcornaz@users.noreply.github.com>
2018-07-21 15:06:26 +0200

    mapping argument for openSubscription (#46)
    * Write test
    * Write implementation
    * Fix tests
    * Update changelog

--- core/common/src/test/kotlin/com/github/jcornaz/miop/experimental/operator/OperatorTest.kt
+++ core/common/src/test/kotlin/com/github/jcornaz/miop/experimental/operator/OperatorTest.kt
@@ -4,6 +4,7 @@ import com.github.jcornaz.miop.internal.test.assertThrows
 import com.github.jcornaz.miop.internal.test.runTest
 import kotlinx.coroutines.experimental.channels.Channel
 import kotlinx.coroutines.experimental.channels.ReceiveChannel
+import kotlin.test.Ignore
 import kotlin.test.Test
 
 abstract class OperatorTest {
@@ -11,6 +12,7 @@ abstract class OperatorTest {
     abstract fun ReceiveChannel<Int>.operator(): ReceiveChannel<Int>
 
     @Test
+    @Ignore // wait on https://github.com/Kotlin/kotlinx.coroutines/issues/415
     fun testCancel() = runTest {
         val source = Channel<Int>()
         source.operator().cancel(DummyException("something went wrong"))
