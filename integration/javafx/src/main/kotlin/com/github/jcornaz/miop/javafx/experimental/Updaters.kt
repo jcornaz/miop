619caa04640b22d5f27a0cfb17d5c7bec37fa027
Jonathan Cornaz <jonathan.cornaz@gmail.com>
2018-07-28 10:52:55 +0200

    Update comment and add an `launchFxSetUpdater` alias

--- integration/javafx/src/main/kotlin/com/github/jcornaz/miop/javafx/experimental/Updaters.kt
+++ integration/javafx/src/main/kotlin/com/github/jcornaz/miop/javafx/experimental/Updaters.kt
@@ -3,7 +3,6 @@ package com.github.jcornaz.miop.javafx.experimental
 import com.github.jcornaz.miop.experimental.distinctUntilChanged
 import com.github.jcornaz.miop.experimental.launchConsumeEach
 import javafx.beans.property.Property
-import javafx.collections.ObservableList
 import kotlinx.coroutines.experimental.Job
 import kotlinx.coroutines.experimental.channels.ReceiveChannel
 import kotlinx.coroutines.experimental.channels.consumeEach
@@ -12,26 +11,26 @@ import kotlinx.coroutines.experimental.javafx.JavaFx
 import kotlinx.coroutines.experimental.launch
 
 /**
- * Start a new job which update the [property] with each elements received
+ * Start a new job in the JavaFx thread which update the [target] with each elements received
  *
  * The result or the [parent] shall be cancelled in order to cancel the channel
  */
-public fun <T> ReceiveChannel<T>.launchFxUpdater(property: Property<in T>, parent: Job? = null): Job =
-    distinctUntilChanged().launchConsumeEach(JavaFx, javafxStart(), parent) { property.value = it }
+public fun <T> ReceiveChannel<T>.launchFxUpdater(target: Property<in T>, parent: Job? = null): Job =
+    distinctUntilChanged().launchConsumeEach(JavaFx, javafxStart(), parent) { target.value = it }
 
 /**
- * Start a new job which update the [observableList] with each new list received
+ * Start a new job in the JavaFx thread which update the [target] with each new list received
  *
  * The result or the [parent] shall be cancelled in order to cancel the channel
  */
-public fun <E> ReceiveChannel<List<E>>.launchFxListUpdater(observableList: ObservableList<in E>, parent: Job? = null): Job =
+public fun <E> ReceiveChannel<List<E>>.launchFxListUpdater(target: MutableList<in E>, parent: Job? = null): Job =
     launchConsumeEach(JavaFx, javafxStart(), parent) { newList ->
         when {
-            observableList.isEmpty() -> observableList.addAll(newList)
-            newList.isEmpty() -> observableList.clear()
+            target.isEmpty() -> target.addAll(newList)
+            newList.isEmpty() -> target.clear()
             else -> {
                 val sourceIterator = newList.iterator()
-                val targetIterator = observableList.listIterator()
+                val targetIterator = target.listIterator()
                 var index = 0
 
                 while (sourceIterator.hasNext() && targetIterator.hasNext()) {
@@ -45,17 +44,27 @@ public fun <E> ReceiveChannel<List<E>>.launchFxListUpdater(observableList: Obser
                 }
 
                 if (targetIterator.hasNext()) {
-                    observableList.remove(index, observableList.size)
+                    target.subList(index, target.size).clear()
                 } else if (sourceIterator.hasNext()) {
-                    sourceIterator.forEachRemaining { observableList.add(it) }
+                    sourceIterator.forEachRemaining { target.add(it) }
                 }
             }
         }
     }
 
 /**
+ * Start a new job in the JavaFx thread which update the [target] with each new set received
+ *
+ * The result or the [parent] shall be cancelled in order to cancel the channel
+ */
+public fun <E> ReceiveChannel<Set<E>>.launchFxSetUpdater(target: MutableSet<in E>, parent: Job? = null): Job =
+    launchFxCollectionUpdater(target, parent)
+
+/**
  * Start a job in the JavaFx thread which keeps up-to-date the [target] collection.
  * Order of elements is ignored. Only consider the elements and their occurrence count.
+ *
+ * The result or the [parent] shall be cancelled in order to cancel the channel
  */
 public fun <E> ReceiveChannel<Collection<E>>.launchFxCollectionUpdater(target: MutableCollection<E>, parent: Job? = null): Job =
     launch(JavaFx, javafxStart(), parent, consumes()) {
