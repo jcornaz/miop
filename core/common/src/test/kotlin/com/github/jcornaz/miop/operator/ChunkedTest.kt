eabb694f8b4a7ee736fac09f5ebed323cfcc6ec0
Jonathan Cornaz <jcornaz@users.noreply.github.com>
2018-09-22 12:31:09 +0200

    Add windowed and chunked operators (#85)
    * Write tests for chunked and window operators
    * Add implementation of windowed and chunked
    * Update changelog
    * Add test for `windowed` with a step bigger than the size

---
+++ core/common/src/test/kotlin/com/github/jcornaz/miop/operator/ChunkedTest.kt
@@ -0,0 +1,53 @@
+package com.github.jcornaz.miop.operator
+
+import com.github.jcornaz.miop.chunked
+import com.github.jcornaz.miop.openSubscription
+import com.github.jcornaz.miop.receiveChannelOf
+import com.github.jcornaz.miop.test.runTest
+import kotlinx.coroutines.channels.ReceiveChannel
+import kotlinx.coroutines.channels.map
+import kotlinx.coroutines.channels.toList
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class ChunkedTest : OperatorTest() {
+
+    override fun <T> ReceiveChannel<T>.operator(): ReceiveChannel<T> = chunked(1).map { it.first() }
+
+    @Test
+    fun testChunked() = runTest {
+
+        // given
+        val source = receiveChannelOf(1, 2, 3, 4, 5, 6, 7)
+
+        // when
+        val windows = source.chunked(3)
+
+        // then
+        assertEquals(
+            actual = windows.toList(),
+            expected = listOf(
+                listOf(1, 2, 3),
+                listOf(4, 5, 6),
+                listOf(7)
+            )
+        )
+    }
+
+    @Test
+    fun chunkedShouldBeConsistentWithSequenceChunked() = runTest {
+
+        // given
+        val sequence = generateSequence(0) { it + 1 }.take(42)
+        val source = sequence.openSubscription()
+
+        // when
+        val windows = source.chunked(7)
+
+        // then
+        assertEquals(
+            actual = windows.toList(),
+            expected = sequence.chunked(7).toList()
+        )
+    }
+}
---
+++ core/common/src/test/kotlin/com/github/jcornaz/miop/operator/WindowedTest.kt
@@ -0,0 +1,143 @@
+package com.github.jcornaz.miop.operator
+
+import com.github.jcornaz.miop.openSubscription
+import com.github.jcornaz.miop.receiveChannelOf
+import com.github.jcornaz.miop.test.runTest
+import com.github.jcornaz.miop.windowed
+import kotlinx.coroutines.channels.ReceiveChannel
+import kotlinx.coroutines.channels.map
+import kotlinx.coroutines.channels.toList
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class WindowedTest : OperatorTest() {
+
+    override fun <T> ReceiveChannel<T>.operator(): ReceiveChannel<T> = windowed(1, 1).map { it.first() }
+
+    @Test
+    fun testWindowed() = runTest {
+
+        // given
+        val source = receiveChannelOf(1, 2, 3, 4, 5, 6)
+
+        // when
+        val windows = source.windowed(3, 2)
+
+        // then
+        assertEquals(
+            actual = windows.toList(),
+            expected = listOf(
+                listOf(1, 2, 3),
+                listOf(3, 4, 5)
+            )
+        )
+    }
+
+    @Test
+    fun testWindowedWithStepBiggerThanSize() = runTest {
+
+        // given
+        val source = receiveChannelOf(1, 2, 3, 4, 5, 6, 7, 8, 9)
+
+        // when
+        val windows = source.windowed(3, 4, true)
+
+        // then
+        assertEquals(
+            actual = windows.toList(),
+            expected = listOf(
+                listOf(1, 2, 3),
+                listOf(5, 6, 7),
+                listOf(9)
+            )
+        )
+    }
+
+    @Test
+    fun testPartialWindow() = runTest {
+
+        // given
+        val source = receiveChannelOf(1, 2, 3, 4, 5, 6)
+
+        // when
+        val windows = source.windowed(3, 2, true)
+
+        // then
+        assertEquals(
+            actual = windows.toList(),
+            expected = listOf(
+                listOf(1, 2, 3),
+                listOf(3, 4, 5),
+                listOf(5, 6)
+            )
+        )
+    }
+
+    @Test
+    fun windowedWithStepBiggerThanSizeShouldBeConsistentWithSequenceWindow() = runTest {
+
+        // given
+        val sequence = generateSequence(0) { it + 1 }.take(42)
+        val source = sequence.openSubscription()
+
+        // when
+        val windows = source.windowed(3, 4)
+
+        // then
+        assertEquals(
+            actual = windows.toList(),
+            expected = sequence.windowed(3, 4).toList()
+        )
+    }
+
+    @Test
+    fun windowShouldBeConsistentWithSequenceWindow() = runTest {
+
+        // given
+        val sequence = generateSequence(0) { it + 1 }.take(42)
+        val source = sequence.openSubscription()
+
+        // when
+        val windows = source.windowed(4, 2)
+
+        // then
+        assertEquals(
+            actual = windows.toList(),
+            expected = sequence.windowed(4, 2).toList()
+        )
+    }
+
+    @Test
+    fun windowWithPartialShouldBeConsistentWithSequenceWindowWithPartial() = runTest {
+
+        // given
+        val sequence = generateSequence(0) { it + 1 }.take(42)
+        val source = sequence.openSubscription()
+
+        // when
+        val windows = source.windowed(4, 2, true)
+
+        // then
+        assertEquals(
+            actual = windows.toList(),
+            expected = sequence.windowed(4, 2, true).toList()
+        )
+    }
+
+    @Test
+    fun windowDefaultShouldBeConsistentWithSequenceDefault() = runTest {
+
+        // given
+        val sequence = generateSequence(0) { it + 1 }.take(42)
+        val source = sequence.openSubscription()
+
+        // when
+        val windows = source.windowed(4)
+
+        // then
+        assertEquals(
+            actual = windows.toList(),
+            expected = sequence.windowed(4).toList()
+        )
+    }
+}
