f1db9babfbef2bc245aaee7aacba9e10a4a44698
Jonathan Cornaz <jonathan.cornaz@gmail.com>
2018-09-22 17:01:44 +0200

    Comment

--- integration/collekt/common/src/main/kotlin/com/github/jcornaz/miop/collekt/CollectionEventHandlers.kt
+++ integration/collekt/common/src/main/kotlin/com/github/jcornaz/miop/collekt/CollectionEventHandlers.kt
@@ -6,6 +6,11 @@ import com.github.jcornaz.collekt.emptyPersistentMap
 import com.github.jcornaz.collekt.emptyPersistentSet
 import com.github.jcornaz.miop.collection.*
 
+/**
+ * Returns a new [PersistentMap] having the [event] applied.
+ *
+ * This is essentially a *reducing* function ([PersistentMap], [MapEvent]) -> [PersistentMap]
+ */
 @ExperimentalCollectionEvent
 public operator fun <K, V> PersistentMap<K, V>.plus(event: MapEvent<K, V>): PersistentMap<K, V> = when (event) {
     is MapEntryAdded -> plus(event.key, event.value)
@@ -14,6 +19,11 @@ public operator fun <K, V> PersistentMap<K, V>.plus(event: MapEvent<K, V>): Pers
     MapCleared -> emptyPersistentMap()
 }
 
+/**
+ * Returns a new [PersistentSet] having the [event] applied.
+ *
+ * This is essentially a *reducing* function ([PersistentSet], [MapEvent]) -> [PersistentSet]
+ */
 @ExperimentalCollectionEvent
 public operator fun <E> PersistentSet<E>.plus(event: SetEvent<E>): PersistentSet<E> = when (event) {
     is SetElementAdded -> plus(event.element)
