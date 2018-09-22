05975db45807cb44236dabe2a77692c57ee419fb
Jonathan Cornaz <jonathan.cornaz@gmail.com>
2018-09-22 16:50:47 +0200

    Add test for `PersistentSet.plus(SetEvent)` operator

--- integration/collekt/common/src/test/kotlin/com/github/jcornaz/miop/collekt/CollectionEventHandlersTest.kt
+++ integration/collekt/common/src/test/kotlin/com/github/jcornaz/miop/collekt/CollectionEventHandlersTest.kt
@@ -1,17 +1,15 @@
 package com.github.jcornaz.miop.collekt
 
 import com.github.jcornaz.collekt.emptyPersistentMap
-import com.github.jcornaz.miop.collection.MapCleared
-import com.github.jcornaz.miop.collection.MapEntryAdded
-import com.github.jcornaz.miop.collection.MapEntryRemoved
-import com.github.jcornaz.miop.collection.MapEntryUpdated
+import com.github.jcornaz.collekt.emptyPersistentSet
+import com.github.jcornaz.miop.collection.*
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
 class CollectionEventHandlersTest {
 
     @Test
-    fun test() {
+    fun testMapEvent() {
         var map = emptyPersistentMap<Int, String>()
         assertEquals(emptyMap<Int, String>(), map)
 
@@ -35,5 +33,26 @@ class CollectionEventHandlersTest {
 
         map += MapEntryAdded(1, "test")
         assertEquals(mapOf(1 to "test"), map)
+    }
+
+    @Test
+    fun testSetEvent() {
+        var set = emptyPersistentSet<String>()
+        assertEquals(emptySet<String>(), set)
+
+        set += SetElementAdded("Hello")
+        assertEquals(setOf("Hello"), set)
+
+        set += SetElementAdded("Hello")
+        assertEquals(setOf("Hello"), set)
+
+        set += SetElementAdded("World")
+        assertEquals(setOf("Hello", "World"), set)
+
+        set += SetElementRemoved("non-present-value")
+        assertEquals(setOf("Hello", "World"), set)
+
+        set += SetCleared
+        assertEquals(emptySet<String>(), set)
     }
 }
