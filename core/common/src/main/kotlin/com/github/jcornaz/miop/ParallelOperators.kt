588a2718e46d0003f651de62a4ab103995bc8009
Jonathan Cornaz <jonathan.cornaz@gmail.com>
2018-11-30 20:41:45 +0100

    Comment and update changelog

--- core/common/src/main/kotlin/com/github/jcornaz/miop/ParallelOperators.kt
+++ core/common/src/main/kotlin/com/github/jcornaz/miop/ParallelOperators.kt
@@ -6,6 +6,28 @@ import kotlinx.coroutines.channels.produce
 
 internal expect val defaultConcurrency: Int
 
+/**
+ * Start a parallel pipeline.
+ *
+ * This method will concurrently call [pipe] (given the [concurrency]) and return a channel to which all results are merged.
+ *
+ * Usage example:
+ * ```kotlin
+ * channel
+ *   .filter { it % 2 } // <-- sequential
+ *   .parallel(3) { // <-- Start 3 parallel pipeline
+ *
+ *     // build a pipeline (this lambda will be invoked 3 times concurrently)
+ *     filter { it > 0 }
+ *       .map { it.toString() }
+ *
+ *   } // <-- Join the parallel channels
+ *   .map { it.length } // <-- sequential again
+ *   .consumeEach {
+ *     println(it)
+ *   }
+ * ```
+ */
 @ObsoleteCoroutinesApi
 @UseExperimental(ExperimentalCoroutinesApi::class)
 public fun <T, R> ReceiveChannel<T>.parallel(concurrency: Int = defaultConcurrency, pipe: ReceiveChannel<T>.() -> ReceiveChannel<R>): ReceiveChannel<R> =
