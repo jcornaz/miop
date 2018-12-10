26bcf9db4b1f7499574ef84bef5fe48fd5562882
Jonathan Cornaz <jonathan.cornaz@gmail.com>
2018-12-10 21:14:09 +0100

    Move version management to buildSrc

---
+++ buildSrc/src/main/kotlin/Version.kt
@@ -0,0 +1,11 @@
+object Version {
+    const val KOTLIN = "1.3.10"
+    const val COROUTINES = "1.0.0"
+    const val DOKKA = "0.9.17"
+    const val COLLEKT = "0.0.3"
+
+    const val JUNIT = "4.13-beta-1"
+    const val KLUENT = "1.44"
+    const val MOCKITO = "2.23.4"
+    const val MOCKITO_KOTLIN = "2.0.0"
+}
